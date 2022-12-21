689c15b757e171571492bd5826b7c05b05fff672
scottemac00 <scott.e.mccraney@gmail.com>
2022-12-20 21:58:03 -0500

    thru 12.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -31,3 +31,30 @@ function buildTable(data) {
         );
       });
     }
+// start a new function to handle what to do after an input
+// is given
+function handleClick() {
+  // .select looks for the very first element that matches the 
+  // selector string ("#datetime") (id w/n HTML tag)
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+  // if-statement syntax
+  // if (condition) {code to execute}
+  // Check to see if a date was entered and filter the
+  // data using that date
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+     // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+};
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);    
